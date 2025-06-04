aae1c95761f8a0f4b380ce6d32be96bbe3b830b9
lucasandreds <lucasandreufmg@gmail.com>
2025-06-04 00:38:26 -0300

    Preparação inicial TP2

---
+++ 3 - ESM Forum/esmforum/testes/modelo.test.js
@@ -0,0 +1,25 @@
+const bd = require('../bd/bd_utils.js');
+const modelo = require('../modelo.js');
+
+beforeEach(() => {
+  bd.reconfig('./bd/esmforum-teste.db');
+  // limpa dados de todas as tabelas
+  bd.exec('delete from perguntas', []);
+  bd.exec('delete from respostas', []);
+});
+
+test('Testando banco de dados vazio', () => {
+  expect(modelo.listar_perguntas().length).toBe(0);
+});
+
+test('Testando cadastro de três perguntas', () => {
+  modelo.cadastrar_pergunta('1 + 1 = ?');
+  modelo.cadastrar_pergunta('2 + 2 = ?');
+  modelo.cadastrar_pergunta('3 + 3 = ?');
+  const perguntas = modelo.listar_perguntas(); 
+  expect(perguntas.length).toBe(3);
+  expect(perguntas[0].texto).toBe('1 + 1 = ?');
+  expect(perguntas[1].texto).toBe('2 + 2 = ?');
+  expect(perguntas[2].num_respostas).toBe(0);
+  expect(perguntas[1].id_pergunta).toBe(perguntas[2].id_pergunta-1);
+});
---
+++ 4 - Roteiro de Testes E2E Usando Cypress/roteiro-cypress/cypress/e2e/spec.cy.js
@@ -0,0 +1,71 @@
+describe('TODOMvc App', () => {
+  it('Verifica se app está abrindo', () => {
+    cy.visit('')
+  })
+
+  it('Insere uma tarefa', () => {
+    cy.visit(''); 
+
+    cy.get('[data-cy=todo-input]')
+      .type('TP2 de Engenharia de Software{enter}');
+
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 1) 
+      .first()
+      .should('have.text', 'TP2 de Engenharia de Software'); 
+  });
+
+  it('Insere e deleta uma tarefa', () => {
+    cy.visit('');
+
+    cy.get('[data-cy=todo-input]')
+      .type('TP2 de Engenharia de Software{enter}');
+
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 1);
+
+    cy.get('[data-cy=todos-list] > li [data-cy=remove-todo-btn]')
+      .invoke('show')
+      .click();
+
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 0);
+  });
+
+  it('Filtra tarefas completas e ativas', () => {
+    cy.visit(''); 
+
+    cy.get('[data-cy=todo-input]')
+      .type('TP2 de ES{enter}')
+      .type('Prova de ES{enter}');
+
+    cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
+      .first()
+      .click();
+
+    cy.get('[data-cy=filter-active-link')
+      .click();
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 1)
+      .first()
+      .should('have.text', 'Prova de ES');
+
+    cy.get('[data-cy=filter-completed-link')
+      .click();
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 1)
+      .first()
+      .should('have.text', 'TP2 de ES');
+
+    cy.get('[data-cy=filter-all-link')
+      .click();
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 2);
+  });
+});
