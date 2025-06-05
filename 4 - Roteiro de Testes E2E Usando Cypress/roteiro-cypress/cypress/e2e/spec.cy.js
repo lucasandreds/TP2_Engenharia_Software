f1fd1a3cbd18fa54b7cd4c0eecbf2342ccff4bf1
lucasandreds <lucasandreufmg@gmail.com>
2025-06-05 02:08:00 -0300

    Parte 4 finalizada

--- 4 - Roteiro de Testes E2E Usando Cypress/roteiro-cypress/cypress/e2e/spec.cy.js
+++ 4 - Roteiro de Testes E2E Usando Cypress/roteiro-cypress/cypress/e2e/spec.cy.js
@@ -68,4 +68,99 @@ describe('TODOMvc App', () => {
       .children()
       .should('have.length', 2);
   });
+
+    it('Edição de uma tarefa', () => {
+    cy.visit(''); 
+
+    cy.get('[data-cy=todo-input]')
+      .type('TP2 de Engenharia de Software{enter}');
+
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 1) 
+      .first()
+      .should('have.text', 'TP2 de Engenharia de Software'); 
+
+    cy.get('[data-cy=todos-list] > li label')
+      .dblclick(); 
+     
+    cy.get('[data-cy=todos-list] > li .edit')
+      .clear()
+      .type('TP novo de Engenharia de Software{enter}');
+
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 1) 
+      .first()
+      .should('have.text', 'TP novo de Engenharia de Software'); 
+  });
+
+  it('Marcar todas as tarefas como completas', () => {
+    cy.visit(''); 
+
+    cy.get('[data-cy=todo-input]')
+      .type('TP2 de ES{enter}')
+      .type('Prova de ES{enter}')
+
+    cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
+      .first()
+      .click();
+
+    cy.get('.toggle-all-label')
+      .click();
+
+    cy.get('[data-cy=todos-list] > li')
+      .should('have.class', 'completed');
+    
+    cy.get('[data-cy=filter-completed-link')
+      .click();
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 2)
+
+    cy.get('[data-cy=filter-active-link')
+      .click();
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 0)
+  });
+
+
+  it('Limpar todas as tarefas completas', () => {
+    cy.visit(''); 
+
+    cy.get('[data-cy=todo-input]')
+      .type('TP2 de ES{enter}')
+      .type('Prova de ES{enter}');
+
+    cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
+      .first()
+      .click();
+
+    cy.get('.clear-completed')
+      .click();
+
+    cy.get('[data-cy=filter-active-link')
+      .click();
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 1)
+      .first()
+      .should('have.text', 'Prova de ES');
+
+    cy.get('[data-cy=filter-completed-link')
+      .click();
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 0)
+
+    cy.get('[data-cy=filter-all-link')
+      .click();
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 1)
+      .first()
+      .should('have.text', 'Prova de ES');
+  });
+
 });
